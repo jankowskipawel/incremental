81fc0684d0e9b32943f70fbfe27ea4ab2f0ad20a
Pawel MacOS <karmiwallan@gmail.com>
2019-01-29 22:23:40 +0100

    Init commit

--- main.js
+++ main.js
@@ -1,24 +1,88 @@
-var gameData = {
-  gold: 0,
-  goldPerClick: 1,
-  goldPerClickCost: 10
+let gameData = {
+    stone: 0,
+    stonePerClick: 1,
+
+    copper: 0,
+    copperChance: 0.1,
+    copperPerClick: 1,
+
+    pickTier: 1,
+
+    minePerSecond: 1,
+    update: 0.01,
 }
 
-function mineGold() {
-  gameData.gold += gameData.goldPerClick
-  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+let btnMine = document.querySelector('.mine');
+btnMine.addEventListener('click', mine);
+
+let btnUpgradePick = document.querySelector('.upgradePick');
+// btnMine.btnUpgradePick.addEventListener('click', upgradePick);
+
+let stoneCount = document.querySelector('.stone');
+let copperCount = document.querySelector('.copper');
+
+let resources = ['stone', 'copper']
+
+function mineStone()
+{
+    gameData.stone += gameData.stonePerClick;
+    stoneCount.textContent = gameData.stone;
 }
 
-function buyGoldPerClick() {
-  if (gameData.gold >= gameData.goldPerClickCost) {
-    gameData.gold -= gameData.goldPerClickCost
-    gameData.goldPerClick += 1
-    gameData.goldPerClickCost *= 2
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
-  }
+function mineCopper()
+{
+    gameData.copper += gameData.copperPerClick;
+    copperCount.textContent = gameData.copper;
 }
 
-var mainGameLoop = window.setInterval(function() {
-  mineGold()
-}, 1000)
+function mine()
+{
+    mineStone();
+    if(Math.random() < gameData.copperChance && gameData.pickTier > 1)
+    {
+        mineCopper();
+    }
+}
+
+function minePerSecond()
+{
+    gameData.stone += gameData.minePerSecond;
+    stoneCount.textContent = gameData.stone;
+}
+
+// TODO
+// function upgradePick()
+// {
+//     let requiredResources = {};
+//     for(let i=0; i<pickTier-1; i++)
+//     {
+//         requiredResources.resources[i] = 1000 * gameData.upgradePickMultiplier;
+//     }
+//     btnUpgradePick.textContent = 'Upgrade Pickaxe <br>Cost: ';
+// }
+
+// todo update buttons on refresh
+
+let mainGameLoop = window.setInterval(function() {
+    minePerSecond()
+  }, 1000)
+  
+
+//////////////////////////save game section //////////////////////////////////////
+// let saveGameLoop = window.setInterval(function() {
+//     localStorage.setItem('incrementalSave', JSON.stringify(gameData))
+// }, 15000)
+
+// let savegameJSON = JSON.parse(localStorage.getItem("incrementalSave"));
+//     if (savegameJSON !== null) {
+//     gameData = savegameJSON
+// }
+
+// function saveGame()
+// {
+//     localStorage.setItem('incrementalSave', JSON.stringify(gameData));
+// }
+
+// let btnSaveGame = document.querySelector('.saveGame');
+// btnSaveGame.addEventListener('click', saveGame);
+//////////////////////////save game section //////////////////////////////////////
